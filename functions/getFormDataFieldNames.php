d9c66edfb5c3c76977262e8d089af080d8ab0dd1
Arpit Parmar <arpitparmar5739@gmail.com>
2016-05-18 12:43:30 +0530

    fixed a bug in getFormDataFieldNames function

--- functions/getFormDataFieldNames.php
+++ functions/getFormDataFieldNames.php
@@ -2,7 +2,7 @@
 //require_once 'database_connection.php';
 function getFormDataFieldNames()
 {
-    $query = "SELECT `COLUMN_NAME` FROM `INFORMATION_SCHEMA`.`COLUMNS` WHERE `TABLE_SCHEMA`='feedback_new' AND `TABLE_NAME`='form_data';";
+    $query = "SELECT `COLUMN_NAME` FROM `INFORMATION_SCHEMA`.`COLUMNS` WHERE `TABLE_SCHEMA`='feedback' AND `TABLE_NAME`='form_data';";
     $query_result = mysql_query($query) or die(mysql_error());
     $ColumnNames = array();
     $i = 0;
